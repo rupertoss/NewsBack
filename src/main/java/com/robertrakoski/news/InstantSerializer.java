b50dc726e8724589820641657ddae2c43b0f5292
rupertoss <rakoski.rupertoss@gmail.com>
2018-06-05 12:40:14 +0200

    Create InstantSerializer

---
+++ src/main/java/com/robertrakoski/news/InstantSerializer.java
@@ -0,0 +1,23 @@
+package com.robertrakoski.news;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+class InstantSerializer extends JsonSerializer<Instant> {
+
+    private DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public void serialize(Instant value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException {
+        String str = format.format(value);
+        gen.writeString(str);
+    }
+}
+
+
