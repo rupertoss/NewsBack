d0bc689f1dd2ccf2c36a29eb4fc766d4df31abb8
rupertoss <rakoski.rupertoss@gmail.com>
2018-06-05 11:52:03 +0200

    Initial commit

---
+++ src/main/java/com/robertrakoski/news/NewsBackApplication.java
@@ -0,0 +1,12 @@
+package com.robertrakoski.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsBackApplication.class, args);
+	}
+}
