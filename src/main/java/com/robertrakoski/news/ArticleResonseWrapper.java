a7ecb77ba46ccb586abf5a69069d8dcb91cda1b7
rupertoss <rakoski.rupertoss@gmail.com>
2018-06-05 15:11:42 +0200

    Create Controller, ExceptionController,

--- src/main/java/com/robertrakoski/news/ArticleResonseWrapper.java
+++ src/main/java/com/robertrakoski/news/ArticleResonseWrapper.java
@@ -2,13 +2,13 @@ package com.robertrakoski.news;
 
 import java.util.List;
 
-class ArticleResonseWrapper {
+class ArticleResponseWrapper {
 	
 	private String country;
 	private String category;
 	private List<Article> articles;
 	
-	protected ArticleResonseWrapper(String country, String category, List<Article> articles) {
+	ArticleResponseWrapper(String country, String category, List<Article> articles) {
 		this.country = country;
 		this.category = category;
 		this.articles = articles;
---
+++ src/main/java/com/robertrakoski/news/Controller.java
@@ -0,0 +1,26 @@
+package com.robertrakoski.news;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/news")
+public class Controller {
+	
+	@Autowired
+	ArticleFetcher articleFetcher;
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ArticleResponseWrapper> getStatus() throws Exception {
+		List<Article> articles = articleFetcher.fetchArticles();
+		ArticleResponseWrapper response = new ArticleResponseWrapper("pl", "technology", articles);
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+}
